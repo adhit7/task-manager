fcc6618021490593709a70c18ad35d0642c62224
adhit7 <adhithyanalan@gmail.com>
2024-09-18 23:48:26 +0530

    env updates

--- utils/googleClient.js
+++ utils/googleClient.js
@@ -1,6 +1,8 @@
 import { OAuth2Client } from 'google-auth-library';
+import dotenv from 'dotenv';
 
 // Create and export the Google OAuth2 client instance
+dotenv.config();
 
 const oauth2Client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
